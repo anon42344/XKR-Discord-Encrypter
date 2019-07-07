634cd4b912f81b5dbcaf21a90193a37716a4d41e
anon42344 <52552593+anon42344@users.noreply.github.com>
2019-07-07 03:15:57 +0200

    Update function added - "git clone" implemented in powershell

--- update.ps1
+++ update.ps1
@@ -1,3 +1,8 @@
+.\xkr_discord_encrypter\extrascript.ps1
+
+del xkr_discord_encrypter -R
+
+
 $url = "https://github.com/anon42344/XKR-Discord-Encrypter/archive/master.zip"
 $output = "$PSScriptRoot\master.zip"
 $start_time = Get-Date
@@ -5,10 +10,14 @@ $start_time = Get-Date
 Import-Module BitsTransfer
 Start-BitsTransfer -Source $url -Destination $output
 
-Expand-Archive -Force C:\Users\admin\Downloads\xkr-update\master.zip .
+Expand-Archive -Force master.zip .
 
 del master.zip
 
+
+Move-Item -Path ".\XKR-Discord-Encrypter-master\xkr_discord_encrypter" -Destination "."
+
 del XKR-Discord-Encrypter-master -R
 
-Move-Item -Path "C:\Users\admin\Downloads\xkr-update\XKR-Discord-Encrypter-master\xkr_discord_encrypter" -Destination "C:\Users\admin\Downloads\xkr-update"
+
+pause
